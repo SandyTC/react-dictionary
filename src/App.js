daf9394603a80b7bbed3dae6128bbe8255bb0889
SandyTC <138057110+SandyTC@users.noreply.github.com>
2024-06-25 12:48:15 +0100

    CSS changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import React from "react";
 import "./App.css";
 import logo from "./logo.png";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="main-content">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/SandyTC">Sandy Trebilcock</a>, is
+          open-sourced on{" "}
+          <a href="https://github.com/SandyTC/react-dictionary">Github</a>, and
+          is hosted on{" "}
+          <a href="https://sandys-react-dictionary.netlify.app/">Netlify</a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
