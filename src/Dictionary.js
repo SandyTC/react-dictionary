3e558a05e5f4dc9797437a967f431817365c9d29
SandyTC <138057110+SandyTC@users.noreply.github.com>
2024-06-25 14:24:23 +0100

    Added API call

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {}
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    let apiKey = "cb77c04d96ad2oba3t437155800bf4f6";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
