2457ad5e5e916968048e9c7ec6dc761126796749
SandyTC <138057110+SandyTC@users.noreply.github.com>
2024-06-26 11:49:04 +0100

    Added Synonyms component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export default function App() {
       <div className=" container main-content">
         <header className="App-header"></header>
         <main>
+          <h2 className="text-center">Dictionary App</h2>
           <Dictionary />
         </main>
       </div>
